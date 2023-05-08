c40ebc4437933eb55d1e5423c59f4f5ee5c03f48
jaracil <pepe@diselpro.com>
2023-05-08 23:34:28 +0200

    Add Segwit address support

--- README.md
+++ README.md
@@ -1,22 +1,28 @@
 # btcbrain
+
 Transforms a passphrase into a valid BTC wallet.
 
 It uses the SHA256 Hash function to derivate private key from passphrase.
 
 ## Why?
+
 I needed a way to regain access to my wallets without depending on any physical instrument.
 
 ## Install
+
 `npm install -g btcbrain`
 
 ## ... Or run directly
+
 `npx btcbrain`
 
 ## Usage
+
 ```
 btcbrain transforms a passphrase into a valid BTC wallet.
 
 Options:
+-l              Generate legacy address.
 -p              Show private key in WIF format.
 -q              Show also QR code.
 --help, -h      Show help.
@@ -26,8 +32,7 @@ btcbrain -p -q  #Show private key in WIF format and QR code.
 ```
 
 ## Tips
+
 You can access your account with a wallet capable of importing private keys in **wif** format (Wallet Import Format) like [Electrum](https://electrum.org/#download).
 
 Do not use simple phrases as these are vulnerable to dictionary attacks.
-
-
--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 "use strict";
 const promptly = require("promptly");
 const crypto = require("crypto");
-const CoinKey = require("coinkey");
+const bitcoin = require('bitcoinjs-lib');
+const ecc = require('tiny-secp256k1');
+const { ECPairFactory } = require('ecpair');
 const chalk = require("chalk");
 const QRCode = require("qrcode");
 
@@ -11,6 +13,7 @@ const helpString = `
 btcbrain transforms a passphrase into a valid BTC wallet.
 
 Options:
+-l              Generate legacy address.
 -p              Show private key in WIF format.
 -q              Show also QR code.
 --help, -h      Show help.
@@ -20,6 +23,7 @@ btcbrain -p -q  #Show private key in WIF format and QR code.
 
 `
 
+const ECPair = ECPairFactory(ecc);
 
 async function getPass() {
     return await promptly.password("Passphrase:", { replace: "*" })
@@ -28,6 +32,11 @@ async function getPass() {
 async function main() {
     let print_private_key = false;
     let print_qrcode = false;
+    let legacyAddress = false;
+
+    if (process.argv.indexOf("-l") !== -1) {
+        legacyAddress = true;
+    }
 
     if (process.argv.indexOf("-p") !== -1) {
         print_private_key = true;
@@ -43,20 +52,33 @@ async function main() {
     }
 
     let pass = await getPass();
-    var wallet = new CoinKey(crypto.createHash("sha256").update(pass).digest());
 
-    console.log(chalk.yellow("Wallet address: ") + chalk.greenBright(wallet.publicAddress));
+    const keyPair = ECPair.fromPrivateKey(crypto.createHash("sha256").update(pass).digest())
+
+    let address;
+    if (!legacyAddress) {
+        address = bitcoin.payments.p2wpkh({ pubkey: keyPair.publicKey }).address;
+    } else {
+        address = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey }).address;
+    }
+
+
+    console.log(chalk.yellow("Wallet address: ") + chalk.greenBright(address));
     if (print_qrcode) {
-        console.log(await QRCode.toString(wallet.publicAddress, { type: 'terminal' }));
+        console.log(await QRCode.toString(address, { type: 'terminal' }));
     }
 
     if (print_private_key) {
+        let pk = keyPair.toWIF()
+        if (!legacyAddress) {
+            pk = "p2wpkh:" + pk
+        }
         if (print_qrcode) {
             console.log("\n");
         }
-        console.log(chalk.yellow("Private Key: ") + chalk.grey(wallet.privateWif));
+        console.log(chalk.yellow("Private Key: ") + chalk.grey(pk));
         if (print_qrcode) {
-            console.log(await QRCode.toString(wallet.privateWif, { type: 'terminal' }));
+            console.log(await QRCode.toString(pk, { type: 'terminal' }));
         }
     } else {
         console.log(chalk.yellowBright("Private key not printed. Use -p flag to print it or -h to see all options"));
